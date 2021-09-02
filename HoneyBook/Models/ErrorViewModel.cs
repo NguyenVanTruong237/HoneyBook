a002f9eba4eadd2daaa014c0c767f22ac4cb2683
Truong10Ngon <toiiuvn013@gmail.com>
2021-09-02 15:27:05 +0700

    Add project files.

---
+++ HoneyBook/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HoneyBook.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
