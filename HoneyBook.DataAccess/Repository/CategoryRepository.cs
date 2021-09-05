1bbced1a9ba193c172b2c625a9c60276198c9d90
Truong10Ngon <toiiuvn013@gmail.com>
2021-09-05 13:37:42 +0700

    Category Repository

---
+++ HoneyBook.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,29 @@
+﻿using HoneyBook.DataAccess.Data;
+using HoneyBook.DataAccess.Repository.IRepository;
+using HoneyBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HoneyBook.DataAccess.Repository 
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private ApplicationDbContext _db;
+        public CategoryRepository(ApplicationDbContext db) : base (db) //Base db truyền vào ở lớp cha để dùng các chức năng của lớp cha
+        {
+            _db = db;
+        }
+        public void Update(Category category)   
+        {
+            var objfromDb = _db.Categories.FirstOrDefault(s => s.Id == category.Id);
+            if (objfromDb != null)
+            {
+                objfromDb.Name = category.Name;
+                _db.SaveChanges();
+            }
+        }
+    }
+}
