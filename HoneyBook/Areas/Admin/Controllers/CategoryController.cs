4fc1fe4aa5386515634141309d4721290c8b7ba9
Truong10Ngon <toiiuvn013@gmail.com>
2021-09-06 14:59:16 +0700

    ...

--- HoneyBook/Areas/Admin/Controllers/CategoryController.cs
+++ HoneyBook/Areas/Admin/Controllers/CategoryController.cs
@@ -1,8 +1,5 @@
 ﻿using HoneyBook.DataAccess.Repository.IRepository;
-<<<<<<< HEAD
 using HoneyBook.Models;
-=======
->>>>>>> 11ee9ef196c2906550041e9b423395249a9da01e
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
