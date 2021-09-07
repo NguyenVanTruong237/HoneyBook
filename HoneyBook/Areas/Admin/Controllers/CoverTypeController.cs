b086518b84aa7770ff786671a2b5f72151563d11
Truong10Ngon <toiiuvn013@gmail.com>
2021-09-07 14:08:07 +0700

    CRUD CoverType

---
+++ HoneyBook/Areas/Admin/Controllers/CoverTypeController.cs
@@ -0,0 +1,78 @@
+﻿using HoneyBook.DataAccess.Repository.IRepository;
+using HoneyBook.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HoneyBook.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CoverTypeController : Controller
+    {
+        private IUnitOfWork _unitOfWork;
+        public CoverTypeController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        
+        public IActionResult Upsert(int? id)
+        {
+            CoverType coverType = new CoverType();
+            if (id == null)
+            {
+                //create
+                return View(coverType);
+            }
+            coverType = _unitOfWork.CoverType.Get(id.GetValueOrDefault());
+            return View(coverType);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(CoverType coverType)
+        {
+            if (ModelState.IsValid) 
+            {
+                var objInDb = _unitOfWork.CoverType.Get(coverType.Id);
+                if (coverType.Id == 0)
+                {
+                    _unitOfWork.CoverType.Add(coverType);
+                }
+                else
+                {
+                    _unitOfWork.CoverType.update(coverType);
+                }
+                _unitOfWork.save();
+                return RedirectToAction(nameof(Index));
+            }
+            return View();
+        }
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+           var allObj = _unitOfWork.CoverType.GetAll();
+            return Json(new { data = allObj });
+        }
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var objInDb = _unitOfWork.CoverType.Get(id);
+            if (objInDb == null)
+            {
+                return Json(new { success = false, message = "Không thành công." });
+            }
+            _unitOfWork.CoverType.Remove(id);
+            _unitOfWork.save();
+            return Json(new { success = true, message = "Xóa thành công." });
+        }
+        #endregion
+    }
+}
---
+++ HoneyBook/wwwroot/js/covertype.js
@@ -0,0 +1,54 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": {
+            "url": "/Admin/CoverType/GetAll",
+        },
+        "columns": [
+            { "data": "name", "width": "20%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `
+                    <a href="CoverType/Upsert/${data}" class="btn btn-primary text-white" style="cursor:pointer">
+                            <i class="far fa-edit"></i>
+                        </a>
+                        <a  onclick=Delete("/Admin/CoverType/Delete/${data}") class="btn btn-danger text-white" style="cursor:pointer">
+                            <i class="fas fa-trash"></i>
+                        </a>`;
+                }
+            }
+        ]
+
+    });
+}
+function Delete(url) {
+    swal({
+        title: "Bạn có muốn xóa không?",
+        text: "Xóa sẽ không khôi phục lại dữ liệu, bạn có chắc chắn?",
+        icon: "warning",
+        buttons: true,
+        dangerMode: true
+    }).then((willDelete) => {
+        if (willDelete) {
+            $.ajax({
+                url: url,
+                type: "DELETE",
+                success: function (data) {
+                    if (data.success) {
+                        toastr.success(data.message);
+                        dataTable.ajax.reload();
+
+                    } else {
+                        toastr.error(data.message);
+                    }
+                }
+            });
+        }
+    });
+}
