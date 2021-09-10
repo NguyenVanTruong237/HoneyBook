5599fe3dbcf349b1850e8ac7b466c24a1dd08c0a
Truong10Ngon <toiiuvn013@gmail.com>
2021-09-10 18:56:00 +0700

    Home view

--- HoneyBook/Areas/Customer/Controllers/HomeController.cs
+++ HoneyBook/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using HoneyBook.Models;
+﻿using HoneyBook.DataAccess.Repository.IRepository;
+using HoneyBook.Models;
 using HoneyBook.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -14,15 +15,18 @@ namespace HoneyBook.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
+        { 
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> products = _unitOfWork.Product.GetAll(includeProPerties: "Category,CoverType");
+            return View(products);
         }
 
         public IActionResult Privacy()
