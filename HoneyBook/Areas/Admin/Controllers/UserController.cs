d25c9e3e75bc9a296a37cae2eb809fe6220bf37c
Truong10Ngon <toiiuvn013@gmail.com>
2021-09-16 01:17:00 +0700

    User Manage

---
+++ HoneyBook/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,48 @@
+﻿using HoneyBook.DataAccess.Data;
+using HoneyBook.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HoneyBook.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public UserController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        #region API CALLS 
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var userList = _db.ApplicationUsers.Include(c => c.Company).ToList();
+            var userRole = _db.UserRoles.ToList();
+            var roleList = _db.Roles.ToList();
+            foreach (var user in userList)
+            {
+                var roleId = userRole.FirstOrDefault(c => c.UserId == user.Id).RoleId;
+                user.Role = roleList.FirstOrDefault(c => c.Id == roleId).Name;
+                if (user.Company == null)
+                {
+                    user.Company = new Company()
+                    {
+                        Name = ""
+                    };
+                }
+            }
+            return Json(new { data = userList });
+        }
+        #endregion
+    }
+}
