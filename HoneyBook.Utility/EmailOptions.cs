5e1cc38d0a9994c450de682a45848a13a93e48da
Truong10Ngon <toiiuvn013@gmail.com>
2021-09-18 13:48:05 +0700

    Send email confirm

---
+++ HoneyBook.Utility/EmailOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HoneyBook.Utility
+{
+    public class EmailOptions
+    {
+        public string SendGridKey { get; set; }
+        public string SendGridUser { get; set; }
+    }
+}
--- HoneyBook/Startup.cs
+++ HoneyBook/Startup.cs
@@ -38,6 +38,7 @@ namespace HoneyBook
             services.AddIdentity<IdentityUser, IdentityRole>().AddDefaultTokenProviders()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddSingleton<IEmailSender, EmailSender>();
+            services.Configure<EmailOptions>(Configuration);
             services.AddScoped<IUnitOfWork, UnitOfWork>();
             services.AddControllersWithViews().AddRazorRuntimeCompilation();
             services.AddRazorPages();
