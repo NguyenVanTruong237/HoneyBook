52d4500cddba07c31ea8ae83f127fcde10060da5
Truong10Ngon <toiiuvn013@gmail.com>
2021-09-20 19:57:48 +0700

    Action ShoppingCarts

---
+++ HoneyBook/Areas/Customer/Controllers/CartController.cs
@@ -0,0 +1,58 @@
+﻿using HoneyBook.DataAccess.Repository.IRepository;
+using HoneyBook.Models.ViewModels;
+using HoneyBook.Utility;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace HoneyBook.Areas.Customer.Controllers
+{
+    [Area("Customer")]
+    public class CartController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IEmailSender _emailSender;
+        private UserManager<IdentityUser> _userManager;
+
+        public CartController(IUnitOfWork unitOfWork, IEmailSender emailSender, UserManager<IdentityUser> userManager)
+        {
+            _unitOfWork = unitOfWork;
+            _emailSender = emailSender;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            var claimIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            var shoppingCartVM = new ShoppingCartVM()
+            {
+                OrderHeader = new Models.OrderHeader(),
+                ShoppingCarts = _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == claim.Value, includeProPerties:"Product")
+            };
+
+            shoppingCartVM.OrderHeader.OrderTotal = 0;
+            shoppingCartVM.OrderHeader.ApplicationUser = _unitOfWork.ApplicationUser.GetFirstOrDefault(u => u.Id == claim.Value, includeProPerties: "Company");
+
+            foreach (var list in shoppingCartVM.ShoppingCarts)
+            {
+                list.Price = SD.GetPriceBaseOnQuantity(list.Count, list.Product.Price, list.Product.Price50, list.Product.Price100);
+                shoppingCartVM.OrderHeader.OrderTotal += (list.Price * list.Count);
+                list.Product.Description = SD.ConvertToRawHtml(list.Product.Description);
+
+                if (list.Product.Description.Length > 100)
+                {
+                    list.Product.Description = list.Product.Description.Substring(0, 99) + "...";
+                }
+            }
+
+            return View(shoppingCartVM);
+        }
+    }
+}
