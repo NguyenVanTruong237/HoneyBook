10ed88f97623007b7ee19341fe25f08914647c1f
Truong10Ngon <toiiuvn013@gmail.com>
2021-09-28 21:55:27 +0700

    More Field For edit Info

--- HoneyBook/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ HoneyBook/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
+using HoneyBook.DataAccess.Repository.IRepository;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -13,13 +15,16 @@ namespace HoneyBook.Areas.Identity.Pages.Account.Manage
     {
         private readonly UserManager<IdentityUser> _userManager;
         private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly IUnitOfWork _unitOfWork;
 
         public IndexModel(
             UserManager<IdentityUser> userManager,
-            SignInManager<IdentityUser> signInManager)
+            SignInManager<IdentityUser> signInManager,
+            IUnitOfWork unitOfWork)
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            _unitOfWork = unitOfWork;
         }
 
         public string Username { get; set; }
@@ -35,18 +40,29 @@ namespace HoneyBook.Areas.Identity.Pages.Account.Manage
             [Phone]
             [Display(Name = "Phone number")]
             public string PhoneNumber { get; set; }
+            [Required]
+            public string StreetAddress { get; set; }
+            [Required]
+            public string City { get; set; }
+            [Required]
+            public string State { get; set; }
+            [Required]
+            public string PostalCode { get; set; }
         }
 
         private async Task LoadAsync(IdentityUser user)
         {
             var userName = await _userManager.GetUserNameAsync(user);
             var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
-
+            var applicationUser = _unitOfWork.ApplicationUser.GetFirstOrDefault(c => c.Id == user.Id, includeProPerties: "Company");
             Username = userName;
-
             Input = new InputModel
             {
-                PhoneNumber = phoneNumber
+                PhoneNumber = phoneNumber,
+                StreetAddress = applicationUser.StreetAddress,
+                City = applicationUser.City,
+                State = applicationUser.State,
+                PostalCode = applicationUser.PostalCode,
             };
         }
 
@@ -77,6 +93,19 @@ namespace HoneyBook.Areas.Identity.Pages.Account.Manage
             }
 
             var phoneNumber = await _userManager.GetPhoneNumberAsync(user);
+            var claimIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimIdentity.FindFirst(ClaimTypes.NameIdentifier);
+            var userInObj = _unitOfWork.ApplicationUser.GetFirstOrDefault(c => c.Id == claim.Value);
+
+            if (ModelState.IsValid)
+            {
+                userInObj.StreetAddress = Input.StreetAddress;
+                userInObj.City = Input.City;
+                userInObj.State = Input.State;
+                userInObj.PostalCode = Input.PostalCode;
+                _unitOfWork.save();
+            }
+
             if (Input.PhoneNumber != phoneNumber)
             {
                 var setPhoneResult = await _userManager.SetPhoneNumberAsync(user, Input.PhoneNumber);
